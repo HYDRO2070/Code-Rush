019537de706620661cb8965e368e923288668eee
shashankiifetech <shashank.pandey@iifetech.com>
2025-06-25 11:57:22 +0530

    Update route.js

--- src/app/api/submit-code/route.js
+++ src/app/api/submit-code/route.js
@@ -96,7 +96,7 @@ function wrapCodeForLanguage(code, language, bottomclass) {
 
 // Function to execute code in Docker (send to the Docker server and handle response)
 async function executeCodeInDocker(code, language) {
-    const dockerServerUrl = 'https://complier-lisj.onrender.com/submit-code'; // Replace with actual Docker server URL
+    const dockerServerUrl = 'https://complier-2wc2.onrender.com/submit-code'; // Replace with actual Docker server URL
 
     try {
         // Send the code to the Docker server for execution
